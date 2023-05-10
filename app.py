23010acd82f0f603f75b49d92f9a216b7eeb0985
avigdorkahalani <avigdor.kahalani@gmail.com>
2023-05-10 11:48:53 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,37 @@
+from urllib import request
+from urllib.request import Request, urlopen
+from bs4 import BeautifulSoup
+import json
 from flask import Flask
+
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-    return 'Hello, World!'
+def get_collection_volume(url):
+    hdr = {"User-Agent": "Mozilla/5.0"}
+
+    req = Request(url, headers=hdr)
+    page_source = urlopen(req).read().decode('utf-8')
+    soup = BeautifulSoup(page_source, "html.parser")
+    html = str(soup.find('html'))
+    volumeIndex = html.find('totalVolume","__typename":"PriceType","unit":"')
+    closingApprIndex = html.find('"',volumeIndex+46)
+    return html[volumeIndex+46:closingApprIndex]
+
+@app.route('/metaroyals_volume')
+def get_metaroyals_volume():
+    MRVolume = get_collection_volume('https://opensea.io/collection/metablaze-metaroyals')
+    MRRoyalties = float(MRVolume)*float(0.1*0.1)
+    return '%f' % MRRoyalties
+
+@app.route('/metagoblins_volume')
+def get_metagoblins_volume():
+    MGVolume = get_collection_volume('https://opensea.io/collection/metablaze-metagoblins')
+    MGRoyalties = float(MGVolume)*float(0.1*0.1)
+    return '%f' % MGRoyalties
+
+@app.route('/total_volume')
+def get_total_volume():
+    MRRoyalties = get_metaroyals_volume()
+    MGRoyalties = get_metagoblins_volume()
+
+    return '%f' % (float(MRRoyalties) + float(MGRoyalties))
